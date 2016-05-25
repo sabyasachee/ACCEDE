9bbc5fda1ba18373f73e9308d784ea7f1d26a34c
sabyasachee <sabyasachee.1301@gmail.com>
2016-05-25 15:37:54 -0700

    Added Matlab script to obtain saliency measures of videos with thresholds 0.2, 0.5 and 0.7

---
+++ saliencyMeasures.m
@@ -0,0 +1,28 @@
+function saliencyMeasures()
+for i=0:20
+    filename = strcat('Documents/video/ACCEDE', sprintf('%05d.mp4', i));
+    outfilename = strcat('ACCEDE', sprintf('%05d.txt', i));
+    fileID = fopen(outfilename, 'w');
+    fprintf(fileID, 'frame_number\t0.2\t0.5\t0.7\n');
+    disp(filename);
+    video = VideoReader(filename);
+%     nframes = video.NumberOfFrames;
+    width = video.Width;
+    heigth = video.Height;
+    area = width * heigth;
+    for j=1:10
+        frame = read(video, j);
+        [L, a, b] = RGB2Lab(frame);
+        salMat = saliencyMeasure({L, a, b});
+        sum_2 = sum(salMat(:)>0.2);
+        sum_5 = sum(salMat(:)>0.5);
+        sum_7 = sum(salMat(:)>0.7);
+        ratio_2 = sum_2/area;
+        ratio_5 = sum_5/area;
+        ratio_7 = sum_7/area;
+        fprintf(fileID, '%d\t%f\t%f\t%f\n', j-1, ratio_2, ratio_5, ratio_7);
+        fprintf('Frame_number %d done\n', j);
+    end
+    fclose(fileID);
+end
+end
